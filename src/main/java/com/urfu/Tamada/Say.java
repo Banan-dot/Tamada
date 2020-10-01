76b40aa16ba38c4d98bfc1583096a29addf04b66
Banan-dot <58605992+Banan-dot@users.noreply.github.com>
2020-10-01 11:17:25 +0500

    Create a bot

---
+++ src/main/java/com/urfu/Tamada/Say.java
@@ -0,0 +1,8 @@
+package main.java.com.urfu.Tamada;
+import net.dv8tion.jda.annotations.ForRemoval;
+
+public class Say {
+    public static void main(String[] args){
+        System.out.println("Cat and dog are friend.");
+    }
+}
