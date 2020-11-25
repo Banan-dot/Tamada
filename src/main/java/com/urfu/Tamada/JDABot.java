9add1c012b58de3623455f1719e0b020bc94620f
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2020-11-25 22:00:00 +0500

    new jda version

--- src/main/java/com/urfu/Tamada/JDABot.java
+++ src/main/java/com/urfu/Tamada/JDABot.java
@@ -3,13 +3,19 @@ package com.urfu.Tamada;
 import com.urfu.Tamada.events.CommandController;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class JDABot {
     public static void main(String[] args) throws Exception {
-        var token = Config.getToken("TOKEN");
-        JDABuilder.createLight(token)
+        JDABuilder.createDefault(Config.getToken("TOKEN"))
+                .setChunkingFilter(ChunkingFilter.ALL) // enable member chunking for all guilds
+                .setMemberCachePolicy(MemberCachePolicy.ALL) // ignored if chunking enabled
+                .enableIntents(GatewayIntent.GUILD_MEMBERS)
                 .addEventListeners(new CommandController())
-                .setActivity(Activity.listening("HUI"))
+                .setActivity(Activity.watching("в будущее России"))
                 .build();
+        System.out.println("Бот запустился...");
     }
 }
