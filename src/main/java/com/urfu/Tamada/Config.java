39a384920f010bbf27d156dcd5aca37165c4670d
Banan-dot <semke.danya@mail.ru>
2020-12-16 18:23:27 +0500

    askdlkasd;l

--- src/main/java/com/urfu/Tamada/Config.java
+++ src/main/java/com/urfu/Tamada/Config.java
@@ -8,8 +8,8 @@ public class Config {
     private static final Dotenv dotenv = Dotenv.load();
     private static final String prefix = "!";
     private static final String botId = "763051961502138410";
-    private static final String url = ".\\resources\\Data.db";
-    private static final String pathToLanguages = ".\\resources\\languages.txt";
+    private static final String url = "./resources/Data.db";
+    private static final String pathToLanguages = "./resources/languages.txt";
     public static String getUrl() { return url; }
     public static String getPathToLanguages() {return pathToLanguages; }
     public static String getBotId(){ return botId;}
