c4dba63c4c1638f0d85cb721a73320fede4afe84
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2020-12-23 21:22:14 +0500

    Annotation + reflection. Paste commands

---
+++ src/main/java/com/urfu/Tamada/command/paste/RemovePasteCommand.java
@@ -0,0 +1,16 @@
+package com.urfu.Tamada.command.paste;
+
+import com.urfu.Tamada.command.Command;
+import com.urfu.Tamada.command.CommandInformation;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+@CommandInformation(name = "rmPaste", information = "Удаляет пасту.", detailedInformation = "[\"Paste name\"]")
+public class RemovePasteCommand extends Command {
+    @Override
+    public void execute(GuildMessageReceivedEvent event) {
+        var pasteCommandInformation = new PasteCommandInformation(event);
+        var pasteDB = PasteCommandInformation.getPastesDB();
+        var guildId = event.getGuild().getIdLong();
+        pasteDB.removePaste(guildId, pasteCommandInformation.getPasteName());
+    }
+}
